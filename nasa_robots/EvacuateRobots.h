ed596e89f8e5053daa9f6191d7327999817c0344
lukkan <patrik.luukkonen@gamil.com>
2012-06-22 09:10:32 +0200

    byta dator

--- nasa_robots/EvacuateRobots.h
+++ nasa_robots/EvacuateRobots.h
@@ -6,9 +6,32 @@ using namespace std;
 
 class EvacuateRobots {
 
+	struct CaveSquare {
+		int row;
+		int col;
+		string cellContent;
+		int oil;
+
+	};//CaveSquare
+
 
 public:
 	string evacuateAll( vector<string> cave, double V) {
+
+		const int R = cave.size();
+		const int C = cave[0].size();
+		vector< vector<CaveSquare> > caveMaze;
+		vector<CaveSquare> tempRow;
+		CaveSquare tempSquare;
+		string tempCh;;
+		for(int r = 0; R < r; r++) {
+
+			for(int c = 0; c < C; c++) {
+				tempCh = cave[r].substr(c,1);
+
+			}//for c
+
+		}//for r
 
 		string ret;
 		ret.append("F");
